0e35459b11899e5e03538eedebbe0eb5405861f1
harrisonst <harrisonsantosdev@gmail.com>
2024-03-10 00:08:06 -0300

    atividades

--- 1A/criacao e desenvolvimento de sistemas/aula-16-modulos-e-matrizes/exemploDeSolucaoAtividadePratica16Video4de4.java
+++ 1A/criacao e desenvolvimento de sistemas/aula-16-modulos-e-matrizes/exemploDeSolucaoAtividadePratica16Video4de4.java
@@ -1,3 +1,37 @@
-public class exemploDeSolucaoAtividadePratica16 {
+//exemplo dado pela professora Mary Luci
+
+import javax.swing.*;
+
+public class exemploDeSolucaoAtividadePratica16Video4de4 
+{
+  public static void soma(int vet[]) 
+  {
+    int s = 0;
+    for (int i = 0; i<vet.length; i++)
+    {
+      s = s + vet[i];
+    }  
+    JOptionPane.showMessageDialog(null, "A somatória é "+ s);
+  }  
+
+  public static int produto(int vet[])
+  {
+    int p = 1;
+    for (int i = 0; i < vet.length; i++)
+    {
+      p = p * vet[i];
+    }  
+    return p;
+  }
   
+  public static void main(String entrada[]) 
+  {
+    int s = 0, vetor[] = {2, 4, 6, 8, 10};
+    int r;
+    
+    soma(vetor);
+    r = produto (vetor);
+    JOptionPane.showMessageDialog(null, "A produtória é " + r);
+    System.exit(0);
+  }
 }
